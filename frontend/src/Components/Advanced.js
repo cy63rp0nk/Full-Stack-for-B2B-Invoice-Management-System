7b4fd8592bc523382611c246fcd9a38992df8489
cy63rp0nk <70837720+cy63rp0nk@users.noreply.github.com>
2022-06-15 18:54:41 +0530

    Delete frontend directory

--- frontend/src/Components/Advanced.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-
-export const loadAdvanced = async (do_id) => {
-    let res = await axios.get(`http://localhost:8080/summa/AdvancedSearch?doc_id=${do_id}`)
-    return res;
-};
